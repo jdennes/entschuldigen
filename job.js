5e82c51b56a7bbeeae67e9d7ac96f5c87d0742c1
James Dennes <jdennes@gmail.com>
2012-10-13 14:19:27 +0200

    Two databases: subscribers and events.

--- job.js
+++ job.js
@@ -1,7 +1,6 @@
 var restify = require('restify');
 
-var db = process.env.CLOUDANT_URL;
-var events = restify.createJsonClient({ url: db });
+var db = restify.createJsonClient({ url: process.env.CLOUDANT_URL });
 
 var api_key = process.env.CM_API_KEY;
 var list_id = process.env.CM_LIST_ID;
@@ -9,17 +8,31 @@ var list_id = process.env.CM_LIST_ID;
 var client = restify.createJsonClient({ url: 'https://api.createsend.com' });
 client.basicAuth(api_key, 'x');
 
-var email = 'example.' + Date.now() + '@example.com';
+var now = Date.now();
+var email = 'example.' + now + '@example.com';
+var name = 'Example ' + now;
 
 // Subscribe
 client.post(
   '/api/v3/subscribers/' + list_id + '.json',
-  { EmailAddress: email, Name: '', CustomFields: [], Resubscribe: true },
+    { EmailAddress: email, Name: name, CustomFields: [], Resubscribe: true },
   function (err, req, res, obj) {
     console.log('Subscribed: %j', obj);
 
-    events.put( // Save event
-      '/events', { type: 'subscribe', email: obj, time: Date.now() },
+    // Add subscriber to local list
+    db.put(
+      '/subscribers/' + email,
+      { state: "Active", name: name, email: email },
+      function (err, req, res, obj) {
+        console.log('Saved %s', email);
+      }
+    );
+
+    // Log event
+    var id = 'subscribe-' + email;
+    db.put(
+      '/events/' + id,
+      { type: 'subscribe', email: obj, time: Date.now() },
       function (err, req, res, obj) {
         console.log("event store save result: %j", obj)
       }
